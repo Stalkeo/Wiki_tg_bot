ded65583414166d108a476b2a13c96ebbb2797d3
Даня Войтович <Stalkeo@users.noreply.github.com>
2023-01-10 18:52:35 +0300

    Update Wiki.py

--- Wiki.py
+++ Wiki.py
@@ -1,36 +1,44 @@
-import telebot
-import random
-from telebot import types
-# Загружаем список интересных фактов
-f = open('facts.txt', 'r', encoding='UTF-8')
-facts = f.read().split('\n')
-f.close()
-# Загружаем список поговорок
-f = open('compliments.txt', 'r', encoding='UTF-8')
-compliments  = f.read().split('\n')
-f.close()
-# Создаем бота
+import telebot, wikipedia, re
+# Создаем экземпляр бота
 bot = telebot.TeleBot('используйте ваш токен')
-# Команда start
+# Устанавливаем русский язык в Wikipedia
+wikipedia.set_lang("ru")
+# Чистим текст статьи в Wikipedia и ограничиваем его тысячей символов
+def getwiki(s):
+    try:
+        ny = wikipedia.page(s)
+        # Получаем первую тысячу символов
+        wikitext=ny.content[:1000]
+        # Разделяем по точкам
+        wikimas=wikitext.split('.')
+        # Отбрасываем всЕ после последней точки
+        wikimas = wikimas[:-1]
+        # Создаем пустую переменную для текста
+        wikitext2 = ''
+        # Проходимся по строкам, где нет знаков «равно» (то есть все, кроме заголовков)
+        for x in wikimas:
+            if not('==' in x):
+                    # Если в строке осталось больше трех символов, добавляем ее к нашей переменной и возвращаем утерянные при разделении строк точки на место
+                if(len((x.strip()))>3):
+                   wikitext2=wikitext2+x+'.'
+            else:
+                break
+        # Теперь при помощи регулярных выражений убираем разметку
+        wikitext2=re.sub('\([^()]*\)', '', wikitext2)
+        wikitext2=re.sub('\([^()]*\)', '', wikitext2)
+        wikitext2=re.sub('\{[^\{\}]*\}', '', wikitext2)
+        # Возвращаем текстовую строку
+        return wikitext2
+    # Обрабатываем исключение, которое мог вернуть модуль wikipedia при запросе
+    except Exception as e:
+        return 'В энциклопедии нет информации об этом'
+# Функция, обрабатывающая команду /start
 @bot.message_handler(commands=["start"])
 def start(m, res=False):
-        # Добавляем две кнопки
-        markup=types.ReplyKeyboardMarkup(resize_keyboard=True)
-        item1=types.KeyboardButton("Факт")
-        item2=types.KeyboardButton("Комплимент")
-        markup.add(item1)
-        markup.add(item2)
-        bot.send_message(m.chat.id, 'Чтоб получить комплимент или узнать о себе факт, используй кнопки ниже:',  reply_markup=markup)
+    bot.send_message(m.chat.id, 'Отправьте мне любое слово, и я найду его значение на Wikipedia')
 # Получение сообщений от юзера
 @bot.message_handler(content_types=["text"])
 def handle_text(message):
-    # Если юзер прислал 1, выдаем ему случайный факт
-    if message.text.strip() == 'Факт' :
-            answer = random.choice(facts)
-    # Если юзер прислал 2, выдаем умную мысль
-    elif message.text.strip() == 'Комплимент':
-            answer = random.choice(compliments)
-    # Отсылаем юзеру сообщение в его чат
-    bot.send_message(message.chat.id, answer)
+    bot.send_message(message.chat.id, getwiki(message.text))
 # Запускаем бота
 bot.polling(none_stop=True, interval=0)
